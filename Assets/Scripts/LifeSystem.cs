adbe358ac5fc51dd4ff0471b6816c5422210acda
Sebras22 <sebastien.branly@efrei.net>
2025-01-15 14:05:40 +0100

    seb commiiiiiiiiiiiiiiiiit

--- Assets/Scripts/LifeSystem.cs
+++ Assets/Scripts/LifeSystem.cs
@@ -1,37 +1,69 @@
 using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+
 public class LifeSystem : MonoBehaviour
 {
     private int maxHeart = 6;
     public int startHeart = 6;
     public int currentHealth;
     private int maxHealth;
-    private int healthPerheart = 2;
+    private int healthPerHeart = 2;
 
     public Image[] healthImages;
-    public Sprite[] healthSprites;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
 
     void Start()
     {
-        currentHealth = startHeart * healthPerheart;
-        maxHealth = maxHeart * healthPerheart;
-        checkHealth();
-
+        currentHealth = startHeart * healthPerHeart;
+        maxHealth = maxHeart * healthPerHeart;
+        UpdateHealthUI();
     }
 
-    void checkHealth()
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+
+    void UpdateHealthUI()
     {
         for (int i = 0; i < maxHeart; i++)
         {
-            if (startHeart <= i)
+            if (i < startHeart)
             {
-                healthImages[i].enabled = false;
+
+                healthImages[i].enabled = true;
+                int heartHealth = Mathf.Clamp(currentHealth - (i * healthPerHeart), 0, healthPerHeart);
+
+                if (heartHealth == healthPerHeart)
+                {
+                    healthImages[i].sprite = fullHeart;
+                }
+                else if (heartHealth > 0)
+                {
+                    healthImages[i].sprite = halfHeart;
+                }
+                else
+                {
+                    healthImages[i].sprite = emptyHeart;
+                }
             }
             else
             {
-                healthImages[i].enabled = true;
+                healthImages[i].enabled = false;
             }
         }
     }
 }
+
+
