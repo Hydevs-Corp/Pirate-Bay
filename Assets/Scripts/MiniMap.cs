a62ac20bf7e19e2a87cf2d763e2bd2dea0bb398b
Sebras22 <sebastien.branly@efrei.net>
2025-01-16 09:56:38 +0100

    wxcw;,kl dvnsbgrusilw

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    void Update()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
